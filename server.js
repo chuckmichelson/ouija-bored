0816e4263b862cd561ee885fe92613fac6a16da7
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-04 16:25:31 -0600

    testing

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const { makeid } = require('./utils');
 
 var express = require('express');
 var app = express();
-var server = require('http').createServer(app);
+var server = require('https').createServer(app);
 var io = require('socket.io').listen(server);
 
 
