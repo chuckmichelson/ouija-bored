fa2f03b5115d9f3b818e84ec9b25f3d3982c49a0
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-04 16:21:59 -0600

    testing

--- server.js
+++ server.js
@@ -11,12 +11,15 @@ const { makeid } = require('./utils');
 
 
 
-const express = require('express');
-const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io').listen(server);
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 
 
+
+server.listen(process.env.PORT || 3000);
+io.listen(process.env.PORT || 3000);
 
 
 const state = {};
@@ -138,6 +141,3 @@ function emitScore(room, score) {
 }
 
 
-
-server.listen(process.env.PORT || 3000);
-io.listen(process.env.PORT || 3000);
