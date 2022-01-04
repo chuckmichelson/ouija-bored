dac4336348a51e83c176110ade1d8d25693c59a1
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-04 16:05:19 -0600

    testing

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ const { makeid } = require('./utils');
 
 
 
-var express = require('express'),
-    app = express(),
-    server = require('http').createServer(app),
-    io = require('socket.io').listen(server),
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server),
 
 
 
