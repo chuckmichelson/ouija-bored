12b78fc365c389e02e165b36ca8c524a21a8317b
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-05 00:50:41 -0600

    testing

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PLANCHETTE_HEIGHT = 120;
 
 console.log("Entering index.js")
 
-const socket = io('https://ouija-bored.herokuapp.com:12345/socket.io/socket.io.js');
+const socket = io('https://ouija-bored.herokuapp.com/');
 // const socket = io.connect('https://ouija-bored.herokuapp.com/');
 
 socket.on('init', handleInit);
