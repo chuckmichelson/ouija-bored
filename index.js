a72d01eafc17ec11a7347666c21662577d2a7e6c
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-05 15:25:41 -0600

    testing

--- index.js
+++ index.js
@@ -112,6 +112,7 @@ function paintGame(state) {
 
 function handleInit(number) {
   console.log("made it to handleInit()")
+  let playerNumber;
   playerNumber = number;
   console.log(playerNumber)
 }
