130f29a3ba16589743501176bd2c89e866765e3b
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-05 10:54:31 -0600

    testing

--- index.js
+++ index.js
@@ -1,154 +1,164 @@
-// const { GRID_SIZE } = require('./constants');
-const FRAME_RATE = 10;
-const CANVAS_WIDTH = 838;
-const CANVAS_HEIGHT = 554;
-const PLANCHETTE_WIDTH = 120;
-const PLANCHETTE_HEIGHT = 120;
-
-console.log("Entering index.js")
-
-const socket = io.connect('https://ouija-bored.herokuapp.com:12345/socket.io/socket.io.js');
-// const socket = io.connect('https://ouija-bored.herokuapp.com/');
+const socket = io('http://localhost:3000');
 
 socket.on('init', handleInit);
-socket.on('gameState', handleGameState);
-socket.on('gameOver', handleGameOver);
-socket.on('gameCode', handleGameCode);
-socket.on('gameScore', handleScore);
-socket.on('unknownCode', handleUnknownCode);
-
-console.log("sockets on")
-
-// const gameScreen = document.getElementById('gameScreen');
-// const initialScreen = document.getElementById('initialScreen');
-// const newGameBtn = document.getElementById('newGameButton');
-const joinGameBtn = document.getElementById('joinGameButton');
-// const gameCodeInput = document.getElementById('gameCodeInput');
-// const gameCodeDisplay = document.getElementById('gameCodeDisplay');
-const numPlayersDisplay = document.getElementById('numPlayersDisplay');
-const scoreDisplay = document.getElementById('scoreDisplay');
 
 
-// joinGame()
-document.getElementById("numPlayersDisplay").innerHTML = 5 + 6;
-joinGameBtn.addEventListener('click', newGame);
-
-newGame()
-
-function newGame() {
-  console.log("made it to NewGame")
-  socket.emit('newGame');
-  init();
+function handleInit(msg) {
+  console.log(msg);
 }
 
-function joinGame() {
-  console.log("made it to joinGame")
-  newGame();
-}
 
-let playerNumber = 1;
-let gameActive = false;
+// // const { GRID_SIZE } = require('./constants');
+// const FRAME_RATE = 10;
+// const CANVAS_WIDTH = 838;
+// const CANVAS_HEIGHT = 554;
+// const PLANCHETTE_WIDTH = 120;
+// const PLANCHETTE_HEIGHT = 120;
 
-function init() {
-  console.log("made it to init")
+// console.log("Entering index.js")
 
-  const layer1 = document.getElementById('layer1');
-  const ctx1 = layer1.getContext('2d');
-  layer1.height = 554;
-  layer1.width = 838;
+// const socket = io.connect('https://ouija-bored.herokuapp.com:12345/socket.io/socket.io.js');
+// // const socket = io.connect('https://ouija-bored.herokuapp.com/');
 
-  const layer2 = document.getElementById('layer2');
-  const ctx2 = layer2.getContext('2d');
-  layer2.height = 554;
-  layer2.width = 838;
+// socket.on('init', handleInit);
+// socket.on('gameState', handleGameState);
+// socket.on('gameOver', handleGameOver);
+// socket.on('gameCode', handleGameCode);
+// socket.on('gameScore', handleScore);
+// socket.on('unknownCode', handleUnknownCode);
 
-  // background image
-  var background = new Image();
-  background.src = "images/ouija_board.png";
-  background.onload = function(){
-      ctx1.drawImage(background,0,0);
-  }
+// console.log("sockets on")
 
-  // planchette
-  var planchette = new Image();
-  planchette.src = "images/planchette.png";
-  planchette.onload = function(){
-      ctx2.drawImage(planchette,CANVAS_WIDTH/2 - PLANCHETTE_WIDTH/2, CANVAS_HEIGHT/2 - PLANCHETTE_HEIGHT/2);
-  }
+// // const gameScreen = document.getElementById('gameScreen');
+// // const initialScreen = document.getElementById('initialScreen');
+// // const newGameBtn = document.getElementById('newGameButton');
+// const joinGameBtn = document.getElementById('joinGameButton');
+// // const gameCodeInput = document.getElementById('gameCodeInput');
+// // const gameCodeDisplay = document.getElementById('gameCodeDisplay');
+// const numPlayersDisplay = document.getElementById('numPlayersDisplay');
+// const scoreDisplay = document.getElementById('scoreDisplay');
 
-  document.addEventListener('keydown', keyDown);
-  console.log("added keydown event listener")
 
-}
+// // joinGame()
+// document.getElementById("numPlayersDisplay").innerHTML = 5 + 6;
+// joinGameBtn.addEventListener('click', newGame);
 
-function keyDown(e) {
-  console.log("made it to keyDown()")
-  socket.emit('keydown', e.keyCode);
-  document.getElementById("scoreDisplay").innerHTML = e.keyCode;
-  console.log("emitted keydown code")
-}
+// newGame()
 
-function paintGame(state) {
-  console.log("made it to paintGameState")
-  ctx2.drawImage(planchette,state.planchette.pos.x - PLANCHETTE_WIDTH/2, state.planchette.pos.y - PLANCHETTE_HEIGHT/2);
-}
+// function newGame() {
+//   console.log("made it to NewGame")
+//   socket.emit('newGame');
+//   init();
+// }
 
-function handleInit(number) {
-  console.log("made it to handleInit()")
-  playerNumber = number;
-}
+// function joinGame() {
+//   console.log("made it to joinGame")
+//   newGame();
+// }
 
-function handleGameState(gameState) {
-  console.log("made it to handleGameState")
+// let playerNumber = 1;
+// let gameActive = false;
 
-  if (!gameActive) {
-    return;
-  }
-  gameState = JSON.parse(gameState);
-  handleScore(gameState.players[0].snake.length)
-  handleNumPlayers(gameState.players.length)
-  requestAnimationFrame(() => paintGame(gameState));
-}
+// function init() {
+//   console.log("made it to init")
 
-function handleGameOver(data) {
-  console.log("made it to handleGameOver()")
-  if (!gameActive) {
-    console.log("game not active")
-    return;
-  }
-  data = JSON.parse(data);
+//   const layer1 = document.getElementById('layer1');
+//   const ctx1 = layer1.getContext('2d');
+//   layer1.height = 554;
+//   layer1.width = 838;
 
-  gameActive = false;
+//   const layer2 = document.getElementById('layer2');
+//   const ctx2 = layer2.getContext('2d');
+//   layer2.height = 554;
+//   layer2.width = 838;
 
-  alert('Game Over');
+//   // background image
+//   var background = new Image();
+//   background.src = "images/ouija_board.png";
+//   background.onload = function(){
+//       ctx1.drawImage(background,0,0);
+//   }
 
-}
+//   // planchette
+//   var planchette = new Image();
+//   planchette.src = "images/planchette.png";
+//   planchette.onload = function(){
+//       ctx2.drawImage(planchette,CANVAS_WIDTH/2 - PLANCHETTE_WIDTH/2, CANVAS_HEIGHT/2 - PLANCHETTE_HEIGHT/2);
+//   }
 
-function handleGameCode(gameCode) {
-  console.log("made it to handleGameCode()")
-  //gameCodeDisplay.innerText = gameCode;
-}
+//   document.addEventListener('keydown', keyDown);
+//   console.log("added keydown event listener")
 
-function handleScore(gameScore) {
-  console.log("made it to handleScore()")
-  scoreDisplay.innerText = gameScore;
-}
+// }
 
-function handleNumPlayers(numPlayers) {
-  console.log("made it to handleNumPlayers()")
-  numPlayersDisplay.innerText = numPlayers;
-}
+// function keyDown(e) {
+//   console.log("made it to keyDown()")
+//   socket.emit('keydown', e.keyCode);
+//   document.getElementById("scoreDisplay").innerHTML = e.keyCode;
+//   console.log("emitted keydown code")
+// }
 
-function handleUnknownCode() {
-  console.log("made it to handleUnknownCode()")
-  reset();
-  alert('Unknown Game Code')
-}
+// function paintGame(state) {
+//   console.log("made it to paintGameState")
+//   ctx2.drawImage(planchette,state.planchette.pos.x - PLANCHETTE_WIDTH/2, state.planchette.pos.y - PLANCHETTE_HEIGHT/2);
+// }
 
-function reset() {
-  console.log("made it to reset()")
-  playerNumber = null;
-  gameCodeInput.value = 'AAAAA';
-  initialScreen.style.display = "block";
-  gameScreen.style.display = "none";
-}
+// function handleInit(number) {
+//   console.log("made it to handleInit()")
+//   playerNumber = number;
+// }
+
+// function handleGameState(gameState) {
+//   console.log("made it to handleGameState")
+
+//   if (!gameActive) {
+//     return;
+//   }
+//   gameState = JSON.parse(gameState);
+//   handleScore(gameState.players[0].snake.length)
+//   handleNumPlayers(gameState.players.length)
+//   requestAnimationFrame(() => paintGame(gameState));
+// }
+
+// function handleGameOver(data) {
+//   console.log("made it to handleGameOver()")
+//   if (!gameActive) {
+//     console.log("game not active")
+//     return;
+//   }
+//   data = JSON.parse(data);
+
+//   gameActive = false;
+
+//   alert('Game Over');
+
+// }
+
+// function handleGameCode(gameCode) {
+//   console.log("made it to handleGameCode()")
+//   //gameCodeDisplay.innerText = gameCode;
+// }
+
+// function handleScore(gameScore) {
+//   console.log("made it to handleScore()")
+//   scoreDisplay.innerText = gameScore;
+// }
+
+// function handleNumPlayers(numPlayers) {
+//   console.log("made it to handleNumPlayers()")
+//   numPlayersDisplay.innerText = numPlayers;
+// }
+
+// function handleUnknownCode() {
+//   console.log("made it to handleUnknownCode()")
+//   reset();
+//   alert('Unknown Game Code')
+// }
+
+// function reset() {
+//   console.log("made it to reset()")
+//   playerNumber = null;
+//   gameCodeInput.value = 'AAAAA';
+//   initialScreen.style.display = "block";
+//   gameScreen.style.display = "none";
+// }
--- server.js
+++ server.js
@@ -1,141 +1,163 @@
-const { FRAME_RATE } = require('./constants');
-const CANVAS_WIDTH = 838;
-const CANVAS_HEIGHT = 554;
-const PLANCHETTE_WIDTH = 120;
-const PLANCHETTE_HEIGHT = 120;
+const io = require('socket.io')();
 
-// const io = require('socket.io')();
-const { initGame, gameLoop, getUpdatedVelocity } = require('./game');
-const { makeid } = require('./utils');
-
-
-const express = require('express')
-const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const PORT = process.env.PORT || 3000;
-
-// http.listen(PORT,function(){
-//     console.log("Listening to port " + PORT);
-// });
-
-
-const state = {};
-const clientRooms = {};
-
-io.on('connection', client => {gameLoop
-
-  client.on('keydown', handleKeydown);
-  client.on('newGame', handleNewGame);
-  client.on('joinGame', handleJoinGame);
-
-  function handleJoinGame(roomName) {
-    console.log("made it to handleJoinGame")
-    const room = io.sockets.adapter.rooms[roomName];
-
-    let allUsers;
-    if (room) {
-      allUsers = room.sockets;
-    }
-
-    let numClients = 0;
-    if (allUsers) {
-      numClients = Object.keys(allUsers).length;
-    }
-    console.log("counted numClients")
-
-    if (numClients === 0) {
-      console.log("numClients = 0")
-      //client.emit('unknownCode');
-      handleNewGame()
-      return;
-    }
-
-    if (numClients === 1) {
-      console.log("numClients = 1")
-      //client.emit('unknownCode');
-      //handleNewGame()
-      return;
-    }
-
-    clientRooms[client.id] = roomName;
-
-    client.join(roomName);
-    // client.number = 2;
-    client.number = 2;
-    client.emit('init', 2);
-
-    startGameInterval(roomName);
-  }
-
-  function handleNewGame() {
-    console.log("made it to handleNewGame")
-    // let roomName = makeid(5);
-    let roomName = 'AAAAA';
-    clientRooms[client.id] = roomName;
-    client.emit('gameCode', roomName);
-
-    state[roomName] = initGame();
-
-    client.join(roomName);
-    client.number = 1;
-    client.emit('init', 1);
-  }
-
-  function handleKeydown(keyCode) {
-    console.log("made it to handleKeydown")
-    const roomName = clientRooms[client.id];
-    if (!roomName) {
-      return;
-    }
-    try {
-      keyCode = parseInt(keyCode);
-    } catch(e) {
-      console.error(e);
-      return;
-    }
-
-    const vel = getUpdatedVelocity(keyCode);
-
-    if (vel) {
-      state[roomName].players[client.number - 1].vel = vel;
-    }
-  }
+io.on('connection', client => {
+  client.emit('init', { data : 'hello world' });
 });
 
-function startGameInterval(roomName) {
-  console.log("made it to startGameInterval")
-  const intervalId = setInterval(() => {
-    const winner = gameLoop(state[roomName]);
 
-    if (!winner) {
-      emitGameState(roomName, state[roomName])
-      // emitScore(roomName, gameScore)
-    } else {
-      emitGameOver(roomName, winner);
-      state[roomName] = null;
-      clearInterval(intervalId);
-    }
-  }, 1000 / FRAME_RATE);
-}
+io.listen(3000)
 
-function emitGameState(room, gameState) {
-  // Send this event to everyone in the room.
-  console.log("made it to emitGameState")
-  io.sockets.in(room)
-    .emit('gameState', JSON.stringify(gameState));
-}
 
-function emitGameOver(room, winner) {
-  console.log("made it to emitGameOver()")
-  io.sockets.in(room)
-    .emit('gameOver', JSON.stringify({ winner }));
-}
 
-function emitScore(room, score) {
-  console.log("made it to emitScore()")
-  io.sockets.in(room)
-    .emit('gameScore', JSON.stringify(gameScore));
-}
 
-io.listen(process.env.PORT || 3000);
+
+
+// const { FRAME_RATE } = require('./constants');
+// const CANVAS_WIDTH = 838;
+// const CANVAS_HEIGHT = 554;
+// const PLANCHETTE_WIDTH = 120;
+// const PLANCHETTE_HEIGHT = 120;
+
+// // const io = require('socket.io')();
+// const { initGame, gameLoop, getUpdatedVelocity } = require('./game');
+// const { makeid } = require('./utils');
+
+
+// const express = require('express')
+// const app = express()
+// const server = require('http').Server(app)
+// const io = require('socket.io')(server)
+// const PORT = process.env.PORT || 3000;
+
+// // http.listen(PORT,function(){
+// //     console.log("Listening to port " + PORT);
+// // });
+
+
+// const state = {};
+// const clientRooms = {};
+
+// app.use("/api", function(req, res, next){
+//    console.log("request handler");
+//     res.end("hello");
+//     console.log(res);
+//     next();
+// });
+
+// io.on('connection', client => {gameLoop
+
+//   console.log(socket.connected); // prints "true"
+//   client.on('keydown', handleKeydown);
+//   client.on('newGame', handleNewGame);
+//   client.on('joinGame', handleJoinGame);
+
+//   function handleJoinGame(roomName) {
+//     console.log("made it to handleJoinGame")
+//     const room = io.sockets.adapter.rooms[roomName];
+
+//     let allUsers;
+//     if (room) {
+//       allUsers = room.sockets;
+//     }
+
+//     let numClients = 0;
+//     if (allUsers) {
+//       numClients = Object.keys(allUsers).length;
+//     }
+//     console.log("counted numClients")
+
+//     if (numClients === 0) {
+//       console.log("numClients = 0")
+//       //client.emit('unknownCode');
+//       handleNewGame()
+//       return;
+//     }
+
+//     if (numClients === 1) {
+//       console.log("numClients = 1")
+//       //client.emit('unknownCode');
+//       //handleNewGame()
+//       return;
+//     }
+
+//     clientRooms[client.id] = roomName;
+
+//     client.join(roomName);
+//     // client.number = 2;
+//     client.number = 2;
+//     client.emit('init', 2);
+
+//     startGameInterval(roomName);
+//   }
+
+//   function handleNewGame() {
+//     console.log("made it to handleNewGame")
+//     // let roomName = makeid(5);
+//     let roomName = 'AAAAA';
+//     clientRooms[client.id] = roomName;
+//     client.emit('gameCode', roomName);
+
+//     state[roomName] = initGame();
+
+//     client.join(roomName);
+//     client.number = 1;
+//     client.emit('init', 1);
+//   }
+
+//   function handleKeydown(keyCode) {
+//     console.log("made it to handleKeydown")
+//     const roomName = clientRooms[client.id];
+//     if (!roomName) {
+//       return;
+//     }
+//     try {
+//       keyCode = parseInt(keyCode);
+//     } catch(e) {
+//       console.error(e);
+//       return;
+//     }
+
+//     const vel = getUpdatedVelocity(keyCode);
+
+//     if (vel) {
+//       state[roomName].players[client.number - 1].vel = vel;
+//     }
+//   }
+// });
+
+// function startGameInterval(roomName) {
+//   console.log("made it to startGameInterval")
+//   const intervalId = setInterval(() => {
+//     const winner = gameLoop(state[roomName]);
+
+//     if (!winner) {
+//       emitGameState(roomName, state[roomName])
+//       // emitScore(roomName, gameScore)
+//     } else {
+//       emitGameOver(roomName, winner);
+//       state[roomName] = null;
+//       clearInterval(intervalId);
+//     }
+//   }, 1000 / FRAME_RATE);
+// }
+
+// function emitGameState(room, gameState) {
+//   // Send this event to everyone in the room.
+//   console.log("made it to emitGameState")
+//   io.sockets.in(room)
+//     .emit('gameState', JSON.stringify(gameState));
+// }
+
+// function emitGameOver(room, winner) {
+//   console.log("made it to emitGameOver()")
+//   io.sockets.in(room)
+//     .emit('gameOver', JSON.stringify({ winner }));
+// }
+
+// function emitScore(room, score) {
+//   console.log("made it to emitScore()")
+//   io.sockets.in(room)
+//     .emit('gameScore', JSON.stringify(gameScore));
+// }
+
+// io.listen(process.env.PORT || 3000);
