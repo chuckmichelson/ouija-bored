e1da3897d617125b333074384a6682b794c4b04b
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-06 11:26:05 -0600

    testing

--- server.js
+++ server.js
@@ -148,7 +148,7 @@ io.on('connection', client => {gameLoop
       console.log("client.number: " + client.number)
       console.log("vel.x: " + vel.x)
       console.log(typeof(state) == 'undefined')
-      console.log("Player 1 Vel: " + state[roomName].players[client.number - 1].vel)
+      console.log("Player 1 Vel: " + state[roomName].players[0].vel)
       state[roomName].players[client.number - 1].vel = vel;
     }
   }
