13c3c69f64836472c40a1da377317162e5cd00c3
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-06 12:32:50 -0600

    multiplayer working

--- server.js
+++ server.js
@@ -134,7 +134,7 @@ io.on('connection', client => {gameLoop
     if (vel) {
       console.log("client.number: " + client.number)
       console.log(typeof(state) == 'undefined')
-      console.log("Player 1 Vel: " + state[roomName].players[0].vel.x)
+      // console.log("Player 1 Vel: " + state[roomName].players[0].vel.x)
       state[roomName].players[client.number - 1].x = vel.x;
       state[roomName].players[client.number - 1].y = vel.y;
     }
