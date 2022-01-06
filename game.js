a801ce30bc740886d6341fec743759a46ff01618
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-06 14:24:38 -0600

    multiplayer working

--- game.js
+++ game.js
@@ -56,9 +56,9 @@ function addPlayer(state) {
   console.log("made it to addPlayer ****************************")
   size = Object.keys([state.players]).length
   console.log("add player size: " + size)  // state.players[size + 1] = { x: 0, y: 0 };
-  state.players[size] = {};
-  state.players[size].x = 0;
-  state.players[size].y = 0;
+  [state.players][size] = {};
+  [state.players][size].x = 0;
+  [state.players][size].y = 0;
 
   // for (let i = 0; i < size; i++) {
   //   console.log("x vel of player " + i + " : " + state.players[i].vel.x)
