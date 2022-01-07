8692b0f1b2711d228e5503466659ef6b76da31d5
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-06 23:44:39 -0600

    testing

--- game.js
+++ game.js
@@ -54,8 +54,8 @@ function addPlayer(state) {
   // state.players[numPlayers + 1] = {};
   // state.players[numPlayers + 1] = { x : 0, y : 0 };
   // state.players.push([ { x : 0, y : 0 } ]);
-  state.players[numPlayers].x = {};
-  state.players[numPlayers].y = {};
+  state.players[numPlayers - 1].x = {};
+  state.players[numPlayers - 1].y = {};
   state.players[numPlayers].x = 0;
   state.players[numPlayers].y = 0;
   numPlayers = keys([state.players]).length;
