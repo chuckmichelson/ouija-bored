c15b964bdfb9c86884dcfb0435dc39f0f2bea6ad
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-07 00:32:44 -0600

    testing

--- game.js
+++ game.js
@@ -36,6 +36,7 @@ function createGameState() {
   console.log("made it to createGameState")
 
   return {
+    numPlayers: 1,
     players: [{ x : 0, y : 0 }],
     planchette: {
       pos: {
@@ -50,23 +51,22 @@ function createGameState() {
 function addPlayer(state) {
   console.log("made it to addPlayer ****************************")
 
-  numPlayers = Object.keys([state['AAAAA'].players]).length;
-  console.log("add player numPlayers: " + numPlayers)  // state.players[size + 1] = { x: 0, y: 0 };
+  // numPlayers = Object.keys([state['AAAAA'].players]).length;
+  console.log("add player numPlayers: " + state.numPlayers)  // state.players[size + 1] = { x: 0, y: 0 };
   // state.players[numPlayers + 1] = {};
   // state.players[numPlayers + 1] = { x : 0, y : 0 };
   // state.players.push([ { x : 0, y : 0 } ]);
   console.log(state['AAAAA'].players == undefined);
+  state.numPlayers += 1;
 
-  state['AAAAA'].players[numPlayers].x = {};
-  state['AAAAA'].players[numPlayers].y = {};
-  state['AAAAA'].players[numPlayers].x = 0;
-  state['AAAAA'].players[numPlayers].y = 0;
-  numPlayers = Object.keys([state.players]).length;
-  console.log("add player numPlayers: " + numPlayers)  // state.players[size + 1] = { x: 0, y: 0 };
+  state['AAAAA'].players[numPlayers - 1].x = 0;
+  state['AAAAA'].players[numPlayers - 1].y = 0;
+  // numPlayers = Object.keys([state.players]).length;
+  console.log("add player numPlayers: " + state.numPlayers)  // state.players[size + 1] = { x: 0, y: 0 };
 
-  for (let i = 0; i < numPlayers; i++) {
-    console.log("x vel of player " + i + " : " + state['AAAAA'].players[i].x)
-  }
+  // for (let i = 0; i < numPlayers; i++) {
+  //   console.log("x vel of player " + i + " : " + state['AAAAA'].players[i].x)
+  // }
   return state;
 
 }
