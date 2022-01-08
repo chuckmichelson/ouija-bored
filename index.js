f8d079212a6cf4335606a346066273a375990b38
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-08 11:45:54 -0600

    working on smoke

--- index.js
+++ index.js
@@ -43,8 +43,8 @@ const scoreDisplay = document.getElementById('scoreDisplay');
 document.body.style.backgroundColor = "black";
 // playGif("images/smoke.gif")
 // makeSmoke()
-// joinGame();
 setTimeout(() => { setImageVisible('smoke', 0); }, 2000);
+joinGame();
 
 
 
