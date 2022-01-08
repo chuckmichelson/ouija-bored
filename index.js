df087c92ae73bd4dd0255b97536b2cf15170fa3a
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-08 12:27:08 -0600

    working on smoke

--- index.js
+++ index.js
@@ -45,20 +45,20 @@ document.body.style.backgroundColor = "black";
 // makeSmoke()
 setTimeout(() => { collapseSmoke(); joinGame();}, 2000);
 
-var coll = document.getElementsByClassName("collapsible");
-var i;
+// var coll = document.getElementsByClassName("collapsible");
+// var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var content = this.nextElementSibling;
+//     if (content.style.display === "block") {
+//       content.style.display = "none";
+//     } else {
+//       content.style.display = "block";
+//     }
+//   });
+// }
 
 function collapseSmoke() {
   var x = document.getElementById("myDIV");
