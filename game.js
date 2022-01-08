44ce553479452cc5f6f1c2303a914d4b7b4b62c2
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-07 19:29:02 -0600

    testing

--- game.js
+++ game.js
@@ -77,8 +77,8 @@ function addPlayer(state) {
   numPlayers = [state.players].x.length;
   console.log("addPlayer numPlayers (before push): " + numPlayers);
   // [state.players].push({ x : 0, y : 0 });
-  [state.players].x.push([0];
-  [state.players].y.push([0];
+  [state.players].x.push([0]);
+  [state.players].y.push([0]);
   numPlayers = [state.players].x.length;
   console.log("addPlayer numPlayers (after push): " + numPlayers);
   // state.players[String(state.numPlayers)].x = 0;
