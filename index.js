2f6be22d4ec509b74590c9a514fc370f560f0b26
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-09 01:08:50 -0600

    letters

--- index.js
+++ index.js
@@ -177,7 +177,7 @@ function paintGame(state) {
   right_ctx.fillStyle = 'whitesmoke';
   right_ctx.textAlign = "center";
   // current_letter = state.letters[state.letters.length - 1];
-  current_letter = state.letter_buffer.substr(id.length - 1);
+  current_letter = state.letter_buffer.substr(state.letter_buffer.length - 1);
   if (current_letter != undefined) {
      right_ctx.fillText(current_letter, 50, 80);
   }
