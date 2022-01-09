f48200fc0d9afb9f439125cf46674b9f3f94bed4
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-08 21:12:08 -0600

    page layout

--- server.js
+++ server.js
@@ -174,7 +174,7 @@ function emitGameOver(room, winner) {
 }
 
 function emitScore(room, score) {
-  console.log("made it to emitScore()")
+  console.log("emitScore(): Room: " + room + " Score: " + score)
   io.sockets.in(room)
     .emit('gameScore', JSON.stringify(score));
 }
