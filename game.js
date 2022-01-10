8d2e0d5a04739faf6c494be4df7ea9812cfcc5b8
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-09 18:04:00 -0600

    letters

--- game.js
+++ game.js
@@ -19,16 +19,21 @@ module.exports = {
 function initGame() {
   console.log("made it to initGame()")
   state = createGameState();
-  state = ouijaGoToLetter(state, 'C');
+  // state = ouijaGoToLetter(state, 'C');
   return state;
 }
 
 function createGameState() {
   console.log("made it to createGameState")
+
   return {
     numSpirits: Array(1).fill(0),
     x: Array(100).fill(0),
     y: Array(100).fill(0),
+    // players : {
+    //   x : [0],
+    //   y : [0],
+    // },
     planchette: {
       pos: {
         x: 100,
@@ -40,8 +45,37 @@ function createGameState() {
   };
 }
 
+
 function addPlayer(state) {
   // console.log("made it to addPlayer ****************************")
+
+  // numPlayers = Object.keys([state['AAAAA'].players]).length;
+  // console.log("add player numPlayers: " + state.numPlayers)  // state.players[size + 1] = { x: 0, y: 0 };
+  // state.players[numPlayers + 1] = {};
+  // state.players[numPlayers + 1] = { x : 0, y : 0 };
+  // state.players.push([ { x : 0, y : 0 } ]);
+  // console.log([state.players].x[0] === undefined);
+  // console.log(state)
+  // console.log([state.x])
+  // numPlayers = 100;
+  // console.log("addPlayer numPlayers (before push): " + numPlayers);
+  // // [state.players].push({ x : 0, y : 0 });
+  // // [state.x].push(0);
+  // // [state.y].push(0);
+  // [state.x][numPlayers] = 0;
+  // [state.y][numPlayers] = 0;
+  // numPlayers = state['AAAAA'].x.length;
+  // console.log("addPlayer numPlayers (after push): " + numPlayers);
+  // state.players[String(state.numPlayers)].x = 0;
+  // state.players[String(state.numPlayers)].y = 0;
+  // numPlayers = Object.keys([state.players]).length;
+  // console.log("add player numPlayers: " + state.numPlayers)  // state.players[size + 1] = { x: 0, y: 0 };
+
+  // for (let i = 0; i < numPlayers; i++) {
+  //   console.log("x vel of player " + i + " : " + state.players[i].x)
+  // }
+  // return state;
+
 }
 
 function gameLoop(state) {
@@ -51,6 +85,7 @@ function gameLoop(state) {
   }
 
   // decision rule
+  // console.log("decision rule numPlayers: " + numPlayers)
   for (let i = 0; i < MAX_PLAYERS_PER_ROOM; i++) {
     // console.log("index: " + i)
     if (state.x[i] === 1 ) {
@@ -90,10 +125,14 @@ function gameLoop(state) {
     state.planchette.pos.y = CANVAS_HEIGHT - PLANCHETTE_WIDTH / 2;
   }
 
+  // console.log("state.planchette.pos.x: " + state.planchette.pos.x)
+  // console.log("state.planchette.pos.y: " + state.planchette.pos.y)
+
+
   current_letter = ouijaGetLetter(state);
   state.letter_buffer += current_letter;
 
-  // if the last 50 frames of the letter buffer are the same, then that's an agreed letter
+  // if the last 30 frames of the letter buffer are the same, then that's an agreed letter
   if (state.letter_buffer.length > 50) {
     var last50 = state.letter_buffer.substr(state.letter_buffer.length - 50);
     console.log(last50)
@@ -114,10 +153,9 @@ function gameLoop(state) {
 
   last_agreed = state.agreed_letters.substr(state.agreed_letters.length - 1);
   if(last_agreed == '.') {
-      return true; // means the game is done
+      return true;
   }
-
-  // reset all player velocities to 0 so that user must hold down the key to move
+  // reset all player velocities to 0
   state.x = Array(5).fill(0);
   state.y = Array(5).fill(0);
 
