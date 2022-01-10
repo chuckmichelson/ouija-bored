3782bd8a362fee0acff8e07c9dd349d8e4038b80
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-10 13:49:56 -0600

    letters

--- index.js
+++ index.js
@@ -220,15 +220,17 @@ function handleGameOver(data) {
   // game over messages
 
 
-  const layer2 = document.getElementById('layer2');
-  const ctx2 = layer2.getContext('2d');
-  ctx2.globalAlpha = 0.2;
-
-
   const layer3 = document.getElementById('layer3');
-  const context = layer3.getContext('2d');
-  context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   const ctx3 = layer3.getContext('2d');
+  ctx3.globalAlpha = 0.2;
+  ctx3.fillStyle = 'black';
+  ctx3.clearRect(0, 0, 838, 48);
+
+
+  const layer4 = document.getElementById('layer4');
+  const context = layer4.getContext('2d');
+  context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  const ctx4 = layer4.getContext('2d');
   ctx3.fillStyle = "white";
   ctx3.textAlign = "center";
   ctx3.font = "120px Copperplate, Papyrus, fantasy";
