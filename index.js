d287226dcef433a3391f19c4bd6d972262a20629
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-09 23:47:03 -0600

    letters

--- index.js
+++ index.js
@@ -169,10 +169,7 @@ function paintGame(state) {
   const right_ctx = right_layer2.getContext("2d");
   right_ctx.clearRect(0, 0, 100, 554);
   right_ctx.font = "60px Copperplate, Papyrus, fantasy";
-
-
   right_ctx.fillStyle = 'rgba(255, 165, 0, 1)';
-
   right_ctx.textAlign = "center";
   // current_letter = state.letter_buffer.substr(state.letter_buffer.length - 1);
   current_letter = state.letter_buffer[state.letter_buffer.length - 1]
@@ -185,7 +182,7 @@ function paintGame(state) {
   console.log("streak: " + streak)
   font_multiplier = streak / state.letter_buffer.length;
   console.log("font_multiplier: " + font_multiplier)
-  right_ctx.fillStyle = 'rgba(255, 255, 255, 1)';
+  right_ctx.fillStyle = 'rgba(255, 255, 255, font_multiplier)';
 
   // display agreed letters
   const layer_agreed = document.getElementById("layer_agreed");
