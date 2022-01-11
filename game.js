3e2b9b6f56c2bf8db1c8139cf39a1df660923b81
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-11 11:19:36 -0600

    num spirits

--- game.js
+++ game.js
@@ -25,7 +25,7 @@ function initGame() {
 
 function createGameState() {
   return {
-    numSpirits: Array(1).fill(0),
+    numSpirits: [0],
     x: Array(100).fill(0),
     y: Array(100).fill(0),
     planchette: {
