cf7a1924ac1114ab46039c5b8c04ccde7b7c1aa3
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-13 12:03:43 -0600

    fix num players display

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
 
 // ***** CODE THAT WORKS FOR LOCALHOST *********************
-// // Load HTTP module
-// const http = require("http");
-// const hostname = '127.0.0.1';
-// const PORT = 3000;
+// Load HTTP module
+const http = require("http");
+const hostname = '127.0.0.1';
+const PORT = 3000;
 
-// //Create HTTP server and listen on port 3000 for requests
-// const server = http.createServer((req, res) => {
+//Create HTTP server and listen on port 3000 for requests
+const server = http.createServer((req, res) => {
 
-//   //Set the response HTTP header with HTTP status and Content type
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello World\n');
-// });
+  //Set the response HTTP header with HTTP status and Content type
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
 
-// const io = require('socket.io')();
+const io = require('socket.io')();
 
-// //listen for request on port 3000, and as a callback function have the port listened on logged
-// io.listen(process.env.PORT || 3000);
-// server.listen(PORT, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${PORT}/`);
-// });
+//listen for request on port 3000, and as a callback function have the port listened on logged
+io.listen(process.env.PORT || 3000);
+server.listen(PORT, hostname, () => {
+  console.log(`Server running at http://${hostname}:${PORT}/`);
+});
 // *********************************************************
 
 
 // ***** LITERALLY THE ONLY CODE IN THE SNAKES EXAMPLE *****
-const io = require('socket.io')();
-io.listen(process.env.PORT || 3000);
+// const io = require('socket.io')();
+// io.listen(process.env.PORT || 3000);
 // *********************************************************
 
 
