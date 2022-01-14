972ad0e19e40305689b1bff1aa4b60ea772b4a00
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-14 15:46:15 -0600

    fix gameover crash

--- index.js
+++ index.js
@@ -106,8 +106,8 @@ function init() {
   // planchette
   var planchette = new Image();
   planchette.src = "images/planchette.png";
-  planchette.onload = function(){
-      ctx2.drawImage(planchette,CANVAS_WIDTH/2 - PLANCHETTE_WIDTH/2, CANVAS_HEIGHT/2 - PLANCHETTE_HEIGHT/2);
+  // planchette.onload = function(){
+  //     ctx2.drawImage(planchette,CANVAS_WIDTH/2 - PLANCHETTE_WIDTH/2, CANVAS_HEIGHT/2 - PLANCHETTE_HEIGHT/2);
   }
 
   document.addEventListener('keydown', keyDown);
