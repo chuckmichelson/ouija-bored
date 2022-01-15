449bd1213dc086ee4f3a5b47d8f2239c6af2ee22
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-15 13:49:57 -0600

    feat gameover screen

--- index.js
+++ index.js
@@ -32,22 +32,7 @@ document.body.style.backgroundColor = "black";
 // playGif("images/smoke.gif")
 // makeSmoke()
 setTimeout(() => { collapseSmoke(); joinGame();}, 2000);
-// setTimeout(() => { joinGame();}, 2000);
 
-// var coll = document.getElementsByClassName("collapsible");
-// var i;
-
-// for (i = 0; i < coll.length; i++) {
-//   coll[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     var content = this.nextElementSibling;
-//     if (content.style.display === "block") {
-//       content.style.display = "none";
-//     } else {
-//       content.style.display = "block";
-//     }
-//   });
-// }
 
 function collapseSmoke() {
   var x = document.getElementById("myDIV");
